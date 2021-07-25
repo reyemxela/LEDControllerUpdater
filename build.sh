917f6bf6fc24e7648447eb1f0ad238218c974f68
Alex Meyer <alexwreyem@gmail.com>
2021-07-24 20:38:34 -0400

    add some auto-packaging

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-fyne-cross linux
-fyne-cross windows -console
-fyne-cross darwin -app-id LEDControllerUpdater
+VERSION=$(cat main.go|grep "APP_VERSION" |cut -d '"' -f 2 |tr -d "v")
+MAINDIR=$PWD
+
+fyne-cross linux -app-version $VERSION
+fyne-cross windows -console -app-version $VERSION
+fyne-cross darwin -app-id LEDControllerUpdater -app-version $VERSION
+
+cd $MAINDIR/fyne-cross/bin/linux-amd64/
+rm $MAINDIR/releases/LEDControllerUpdater_linux.tar.gz
+tar -czf $MAINDIR/releases/LEDControllerUpdater_linux.tar.gz LEDControllerUpdater
+
+cd $MAINDIR/fyne-cross/dist/windows-amd64/
+rm $MAINDIR/releases/LEDControllerUpdater_windows.zip
+cp LEDControllerUpdater.exe.zip $MAINDIR/releases/LEDControllerUpdater_windows.zip
+
+cd $MAINDIR/fyne-cross/dist/darwin-amd64/
+rm $MAINDIR/releases/LEDControllerUpdater_mac.zip
+zip -r $MAINDIR/releases/LEDControllerUpdater_mac.zip LEDControllerUpdater.app
+
+cd $MAINDIR
